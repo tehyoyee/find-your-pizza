113721c6599d25b6ace309b97b89f9e940985706
KoreanCow <>
2024-01-26 16:29:32 +0900

    koreancow

--- front-end/src/pages/question-page/QuestionPage.js
+++ front-end/src/pages/question-page/QuestionPage.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './QuestionPage.scss'
 
-const QuestionPage = () => {
+const QuestionPage = ({ UUID }) => {
+  const [question, setQuestion] = useState('');
+  useEffect(() => {
+    // axios.get('http://localhost:5000/question', {
+    //  
+    // }).then(r => r);
+
+  }, [question])
+
+  const onSubmithandler = (e) => {
+    e.preventDefault();
+    // axios.post('http://localhost:5000/question', {}).then
+  }
+
   return (
-    <div>
+    <div className='question-maincontainer'>
+      <div className='question-block'>
+        <p>{question}</p>
+        <form onSubmit={onSubmithandler}>
 
+          <button className='question-box' type='submit'>
+            {/* {first_question} */}
+          </button>
+
+          <button className='question-box' type='submit'>
+            {/* {second_question} */}
+          </button>
+          {/*  map 메소드로 변호나해보기  */}
+        </form>
+      </div>
     </div>
   )
 }
