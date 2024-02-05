88e378e9759518367789d3d2e65a21f47f91c243
YongBin Kim <yongbyn@naver.com>
2024-02-05 10:45:59 +0900

    make MBTI Enum class

---
+++ back-end/src/main/java/com/pizzaTest/repository/MBTI.kt
@@ -0,0 +1,6 @@
+package `back-end`.src.main.java.com.pizzaTest.repository
+
+enum class MBTI {
+
+    INFJ, ENFJ, INFP, ENFP, INTJ, ENTJ, INTP, ENTP, ISFJ, ESFJ, ISTJ, ESTJ, ISFP, ESFP, ISTP, ESTP
+}
