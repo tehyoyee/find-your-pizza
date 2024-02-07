1a7ada979d0164f06af8056c097bcb51fc976d43
KoreanCow <>
2024-02-07 23:09:57 +0900

    Add bottom Images and pizzaflake Evenet to FirstPageComponent.js

--- front-end/src/components/firstpage/FirstPagaComponent.js
+++ front-end/src/components/firstpage/FirstPagaComponent.js
@@ -2,26 +2,41 @@ import React from 'react'
 import '../../pages/first-page/FirstPage.scss'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
-const ImageSrc = '../../Image/피자뽀이.png';
+const ImageSrc = '../../Image/pizzaIcon.png';
 
 const getRandomValue = (max) => `${Math.floor(Math.random() * max)}px`;
 
-const TestStyledComponent = styled.div`
+const snowAnimation = keyframes`
+  0% {
+    transform: translateY(-100vh) rotate(0deg);
+  }
+  100% {
+    transform: translateY(100vh) rotate(360deg);
+  }
+`;
+
+const Snow = styled.div`
   top: ${props => props.top};
-  right: ${props => props.right};
-  width: 150px;
-  height: 60px;
+  left: ${props => props.left};
+  width: 30px;
+  height: 30px;
+  
+  background-image: url(${ImageSrc});
+  background-size: contain; /* 이미지 크기에 맞게 조정 */
+  background-repeat: no-repeat; /* 이미지 반복 없음 */
+  z-index: -1;
   position: absolute;
-  background-image: url(${props => props.image});
-  background-size: cover;
   z-index: 0;
+  animation: ${snowAnimation} ${props => props.animationDuration}s linear infinite;
 `;
 
 
 
+
 const FirstPagaComponent = () => {
+
   const navigate = useNavigate();
   const onStartBtn = async () => {
     try {
@@ -32,34 +47,29 @@ const FirstPagaComponent = () => {
       console.log(e);
     }
   }
-  const components = Array.from({ length: 3 }).map((_, index) => {
-    const top = getRandomValue(window.innerHeight - 60); // 높이에서 60px 뺀 값으로 설정
-    const right = getRandomValue(window.innerWidth - 150); // 너비에서 150px 뺀 값으로 설정
+
+  const snowflakes = Array.from({ length: 15 }).map((_, index) => {
+    const top = getRandomValue(window.innerHeight);
+    const left = getRandomValue(window.innerWidth);
+    const animationDuration = Math.random() * 15 + 15;
     return (
-      <TestStyledComponent
-        key={index}
-        image={ImageSrc}
-        top={top}
-        right={right}
-      />
+      <Snow key={index} top={top} left={left} animationDuration={animationDuration} />
     );
   });
   return (
     <div className="main-container">
-      {components}
-
-
-      <img className='test-img' src="../../Image/피자뽀이.png" alt="피자뽀이" />
+      {snowflakes}
+      <img className='test-img' src="../../Image/pizzaBoy.png" alt="피자뽀이" />
       <div className="main-top">
         <p className="main-title">내가 만약 <span>피자</span>라면?</p>
-        <img className='main-title-img' src="../../Image/피자뽀이.png" alt="피자뽀이" />
+        <img className='main-title-img' src="../../Image/pizzaBoy.png" alt="피자뽀이" />
         <button
           className='start-btn'
           onClick={onStartBtn}
         >시작하기</button>
       </div>
       <div className='main-bottom'>
-        <img className='main-bottom-img' src="../../Image/피자결과뽀이.png" alt="피자뽀이" />
+        <img className='main-bottom-img' src="../../Image/djSimpson.png" alt="피자뽀이" />
       </div>
     </div>
   )
