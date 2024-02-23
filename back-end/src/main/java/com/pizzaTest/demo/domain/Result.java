80bd44b02712924e1e353720cdf4369aaeffa851
YongBin Kim <yongbyn@naver.com>
2024-02-23 12:32:18 +0900

    add: resultDescription for Mysql Error (data too long)

--- back-end/src/main/java/com/pizzaTest/demo/domain/Result.java
+++ back-end/src/main/java/com/pizzaTest/demo/domain/Result.java
@@ -16,8 +16,13 @@ public class Result {
     private Long id;
     private String resultTitle;
     private String resultSubTitle;
+
+    // desciption 길이 오류 ERROR 1406: 1406: Data too long for column 'result_description' at row 1
+    @Column(length = 500)
     private String resultDescription;
     private int[] selectQuestion;
+
+    // Eunum 타입 String 변환
     @Enumerated(EnumType.STRING)
     private MBTI mbti;
 
