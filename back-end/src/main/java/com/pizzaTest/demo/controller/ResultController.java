1c4bf57471d3e92b8b47e45646c861ccced9f4ca
YongBin Kim <yongbyn@naver.com>
2024-02-24 01:28:34 +0900

    test

--- back-end/src/main/java/com/pizzaTest/demo/controller/ResultController.java
+++ back-end/src/main/java/com/pizzaTest/demo/controller/ResultController.java
@@ -24,6 +24,7 @@ public class ResultController {
         Cookie[] cookies = request.getCookies();
         String uuid = cookies[0].getValue();
 
+
         return resultService.sendResult(uuid);
 
     }
