95444c145fbc51a1b91c246698d1697044788786
Hayoung Shim <gkdud3579@gmail.com>
2024-03-18 18:59:17 +0900

    Chore: TitleContainer dummy Data 삭제

--- front-end/src/pages/result-page/components/TitleContainer.js
+++ front-end/src/pages/result-page/components/TitleContainer.js
@@ -9,19 +9,19 @@ const TitleContainer = ({ formData }) => {
         src="Image/pizzaResultBoy.png"
         alt="pizza delivery"
       />
-      {/* <span className="Title">
+      <span className="Title">
         <span className="TitleText">{formData.resultSubTitle}</span>
         <span className="TitleTextGroup">
           <span className="TitleText">{formData.resultTitle}</span>
           <span className="TitleText">같은 당신</span>
         </span>
-      </span> */}
-      <span className="Title">
+      {/* </span> */}
+      {/* <span className="Title">
         <span className="TitleText">으스스한 매콤함</span>
         <span className="TitleTextGroup">
           <span className="TitleText">고스트 페퍼피자</span>
           <span className="TitleText">같은 당신</span>
-        </span>
+        </span> */}
       </span>
     </div>
   );
