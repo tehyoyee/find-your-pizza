dabcecef9f62b6db617b0d4245e95ab2a759ad93
KoreanCow <vue-cli@kakao.com>
2024-03-18 12:01:44 +0900

    Styles: Button Style add active event

--- front-end/src/pages/first-page/FisrtPage.js
+++ front-end/src/pages/first-page/FisrtPage.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import FirstPagaComponent from './components/FirstPagaComponent'
+import React, { useEffect } from 'react';
 import PizzaFlakes from './components/PizzaFlakes';
+import styles from './FirstPage.module.scss';
+import StartBtn from './components/StartBtn';
+
+import { useCookies } from 'react-cookie';
+
 const FirstPage = () => {
+  const [cookies, setCookies, removeCookie] = useCookies(['uuid'])
+
+  useEffect(() => {
+    removeCookie('uuid');
+  }, [])
+
   return (
-    <>
-      <FirstPagaComponent />
+    <div id='root' className={styles.root} >
+
+      <div className={styles.FirstPageContainer}>
+
+        <div className={styles.FirstPageTop}>
+          <div style={{ width: '100%', height: '100px' }} />
+          <div className='Images'>
+            <img className={styles.MainPizzaBoy} src='Image/pizzaBoy.png' alt='PizzaBoy' />
+            <img className={styles.MainPizzaIcon1} src='Image/pizzaIcon.png' alt='PizzaIcon' />
+            <img className={styles.MainPizzaIcon2} src='Image/pizzaIcon.png' alt='PizzaIcon' />
+          </div>
+          <p>내가 만약 <span>피자</span>라면?</p>
+          <img className={styles.FirstPageMainImg} src='Image/pizzaBoy.png' alt='PizzaBoy' />
+          <StartBtn />
+        </div>
+        <div className={styles.FirstPageBottom}>
+          <img src='Image/djSimpson.png' alt='simpsonPizza' />
+        </div>
+      </div>
       <PizzaFlakes />
-    </>
+    </div>
   );
 };
 
---
+++ front-end/src/pages/first-page/components/StartBtn.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './StartBtn.scss'
+import { useNavigate } from 'react-router-dom'
+
+const StartBtn = () => {
+  const navigate = useNavigate();
+
+  const onClickHandler = async (e) => {
+    e.preventDefault();
+    try {
+      navigate('question');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return (
+    <button className='btn yellow' onClick={onClickHandler}>시작하기</button>
+  )
+}
+
+export default StartBtn
