09b22fad8bab03f00efcd20fbe7ffd73b901110f
Hayoung Shim <gkdud3579@gmail.com>
2024-03-19 15:07:16 +0900

    Design: ResultPage 버튼간격조정 및 타이틀위치조정

--- front-end/src/pages/result-page/components/TitleContainer.js
+++ front-end/src/pages/result-page/components/TitleContainer.js
@@ -12,17 +12,21 @@ const TitleContainer = ({ formData }) => {
       <span className="Title">
         <span className="TitleText">{formData.resultSubTitle}</span>
         <span className="TitleTextGroup">
-          <span className="TitleText">{formData.resultTitle}</span>
+          <span className="TitleText" style={{ color: "#FF7A00", fontSize: "23px" }}>
+            {formData.resultTitle}
+          </span>
           <span className="TitleText">같은 당신</span>
         </span>
-      {/* </span> */}
+      </span>
       {/* <span className="Title">
         <span className="TitleText">으스스한 매콤함</span>
         <span className="TitleTextGroup">
-          <span className="TitleText">고스트 페퍼피자</span>
+          <span className="TitleText" style={{ color: "#FF7A00", fontSize: "23px" }}>
+            고스트 페퍼피자
+          </span>
           <span className="TitleText">같은 당신</span>
-        </span> */}
-      </span>
+        </span>
+      </span> */}
     </div>
   );
 };
