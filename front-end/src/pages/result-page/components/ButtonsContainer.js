8b1a92c8630673b74edfd23c69df453c07491eda
Hayoung Shim <gkdud3579@gmail.com>
2024-03-25 16:07:12 +0900

    Design: 카카오톡 공유 버튼 이미지 삭제

--- front-end/src/pages/result-page/components/ButtonsContainer.js
+++ front-end/src/pages/result-page/components/ButtonsContainer.js
@@ -16,10 +16,6 @@ const ButtonsContainer = ({ onRetry, onCopy }) => {
       </div>
       {/* 카카오톡 공유 기능을 구현할 경우에는 여기에 추가 */}
       <div className="Button" onClick={shareKakao}>
-        <img
-          src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
-          alt="카카오링크 보내기 버튼"
-        />
         카카오톡으로 공유
       </div>
     </div>
