bc7d4927e57fb9a4f004397faed1aac510de44e3
Hayoung Shim <gkdud3579@gmail.com>
2024-03-25 16:00:40 +0900

    Feat: 카카오톡 url 공유 기능 구현

--- front-end/src/pages/result-page/components/ButtonsContainer.js
+++ front-end/src/pages/result-page/components/ButtonsContainer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-
 const ButtonsContainer = ({ onRetry, onCopy }) => {
+  const shareKakao = () => {
+    window.Kakao.Link.sendCustom({
+      templateId: 105921, // 내가 만든 템플릿 아이디를 넣어주면 된다
+    });
+  };
   return (
     <div className="ButtonsContainer">
       <div className="Button" onClick={onRetry}>
@@ -11,7 +15,13 @@ const ButtonsContainer = ({ onRetry, onCopy }) => {
         URL 복사하기
       </div>
       {/* 카카오톡 공유 기능을 구현할 경우에는 여기에 추가 */}
-      <div className="Button">카카오톡으로 공유</div>
+      <div className="Button" onClick={shareKakao}>
+        <img
+          src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
+          alt="카카오링크 보내기 버튼"
+        />
+        카카오톡으로 공유
+      </div>
     </div>
   );
 };
