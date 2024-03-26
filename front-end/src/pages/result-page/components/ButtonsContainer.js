bc84d37103a578f881de3cc5d6299ee6b9305fbe
Hayoung Shim <gkdud3579@gmail.com>
2024-03-26 21:02:03 +0900

    Fix: 카카오톡 공유 버튼 오류 수정

--- front-end/src/pages/result-page/components/ButtonsContainer.js
+++ front-end/src/pages/result-page/components/ButtonsContainer.js
@@ -1,29 +1,33 @@
 import React from "react";
 
 const ButtonsContainer = ({ onRetry, onCopy }) => {
-    const shareKakao = () => {
-        // 여기서는 sendDefaultButton을 사용한 예시를 보여줍니다.
-        if (window.Kakao && window.Kakao.isInitialized()) {
-            window.Kakao.Link.sendDefault({
-          objectType: 'feed',
-          content: {
-            title: '내가 만약 피자라면?',
-            description: '간단한 심리테스트를 통해 나와 비슷한 피자를 확인해보세요!',
+  const shareKakao = () => {
+    if (window.Kakao && window.Kakao.isInitialized()) {
+      window.Kakao.Link.sendDefault({
+        objectType: "feed",
+        content: {
+          title: "내가 만약 피자라면?",
+          description:
+            "간단한 심리테스트를 통해 나와 비슷한 피자를 확인해보세요!",
+          link: {
+            mobileWebUrl: "https://www.find-your-pizza.site",
+            webUrl: "https://www.find-your-pizza.site",
+          },
+        },
+        buttons: [
+          {
+            title: "지금 확인하기",
             link: {
-              mobileWebUrl: 'https://www.find-your-pizza.site',
-              androidExecParams: 'test',
+              mobileWebUrl: "https://www.find-your-pizza.site",
+              webUrl: "https://www.find-your-pizza.site",
             },
           },
-          buttons: [
-            {
-              title: '지금 확인하기',
-              link: {
-                mobileWebUrl: 'https://www.find-your-pizza.site',
-              },
-            },
-          ],
-        });
-      };
+        ],
+      });
+    } else {
+      console.log("Kakao SDK not initialized.");
+    }
+  };
 
   return (
     <div className="ButtonsContainer">
