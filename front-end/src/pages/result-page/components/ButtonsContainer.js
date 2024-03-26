eed18baf849766b71fd3325ff059bba626d9bff0
Hayoung Shim <gkdud3579@gmail.com>
2024-03-26 22:03:12 +0900

    Fix: 카카오 공유하기 템플릿으로 적용

--- front-end/src/pages/result-page/components/ButtonsContainer.js
+++ front-end/src/pages/result-page/components/ButtonsContainer.js
@@ -1,33 +1,14 @@
 import React from "react";
-
 const ButtonsContainer = ({ onRetry, onCopy }) => {
-  const shareKakao = () => {
-    if (window.Kakao && window.Kakao.isInitialized()) {
-      window.Kakao.Link.sendDefault({
-        objectType: "feed",
-        content: {
-          title: "내가 만약 피자라면?",
-          description:
-            "간단한 심리테스트를 통해 나와 비슷한 피자를 확인해보세요!",
-          link: {
-            mobileWebUrl: "https://www.find-your-pizza.site",
-            webUrl: "https://www.find-your-pizza.site",
-          },
-        },
-        buttons: [
-          {
-            title: "지금 확인하기",
-            link: {
-              mobileWebUrl: "https://www.find-your-pizza.site",
-              webUrl: "https://www.find-your-pizza.site",
-            },
-          },
-        ],
-      });
-    } else {
-      console.log("Kakao SDK not initialized.");
-    }
-  };
+    const shareKakao = () => {
+        if (window.Kakao && window.Kakao.isInitialized()) {
+          window.Kakao.Link.sendCustom({
+            templateId: '106291',
+          });
+        } else {
+          console.log('Kakao SDK not initialized.');
+        }
+      };
 
   return (
     <div className="ButtonsContainer">
